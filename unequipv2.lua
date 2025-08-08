124b864a2218310a5849337b8c0a355ac0c442fd
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-08 17:16:03 +0100

    Create unequipv2.lua

---
+++ unequipv2.lua
@@ -0,0 +1,10 @@
+for i=1,10 do
+  for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.ScrollingFrame:GetChildren do
+    if v:FindFirstChildWhichIsA("Frame") then
+      if v:FindFirstChildWhichIsA("Frame").Name=="PetTemplate" then return end
+      local petpart = v:FindFirstChildWhichIsA("Part")
+      game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("UnequipPet", petpart.Name)
+    end
+  end
+  task.wait(0.01)
+end
