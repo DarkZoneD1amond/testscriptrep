9b9aedfab9ba0a8483883b273306d40a99295368
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-08 16:52:23 +0100

    Update testingmpets.lua

--- testingmpets.lua
+++ testingmpets.lua
@@ -1,6 +1,6 @@
 for i=1,10 do
   for b,v in game.workspace.PetsPhysical:getChildren() do
-    if v:FindFirstChildWhichIsA("Part" then
+    if v:FindFirstChildWhichIsA("Part") then
       local petpart = v:FindFirstChildWhichIsA("Part")
       game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("UnequipPet", petpart.Name)
       game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("EquipPet", petpart.Name, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
