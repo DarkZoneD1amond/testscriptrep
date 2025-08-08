2c01f358d1bed7d7a5665b03e90bd3b60c21c61f
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-08 16:31:52 +0100

    Create testingmpets.lua

---
+++ testingmpets.lua
@@ -0,0 +1,8 @@
+for i=1,10 do
+  for b,v in game.workspace.PetsPhysical:getChildren() do
+    local petpart = v:FindFirstChildWhichIsA("Part")
+    game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForchild("PetsService"):FireServer("UnequipPet", petpart.Name)
+    game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForchild("PetsService"):FireServer("EquipPet", petpart.Name, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
+  end
+  task.wait(0.01)
+end
