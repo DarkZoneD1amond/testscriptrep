30d487074442a396a3b2471f245502171ab01bfb
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-08 17:16:49 +0100

    Update unequipv2.lua

--- unequipv2.lua
+++ unequipv2.lua
@@ -1,5 +1,5 @@
 for i=1,10 do
-  for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.ScrollingFrame:GetChildren do
+  for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.ScrollingFrame:GetChildren() do
     if v:FindFirstChildWhichIsA("Frame") then
       if v:FindFirstChildWhichIsA("Frame").Name=="PetTemplate" then return end
       local petpart = v:FindFirstChildWhichIsA("Part")
