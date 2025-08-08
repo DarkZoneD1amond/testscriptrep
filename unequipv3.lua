eebc85adc0d3c2115e4e64a723a2c98af4c96792
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-08 17:18:22 +0100

    Update unequipv3.lua

--- unequipv3.lua
+++ unequipv3.lua
@@ -2,7 +2,7 @@ for i=1,10 do
   for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.ScrollingFrame:GetChildren() do
     if v:FindFirstChildWhichIsA("Frame") then
       if v:FindFirstChildWhichIsA("Frame").Name=="PetTemplate" then return end
-      local petpart = v:FindFirstChildWhichIsA("Part")
+      local petpart = v:FindFirstChildWhichIsA("Frame")
       game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("UnequipPet", petpart.Name)
     end
   end
