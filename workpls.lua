356ffcf64239e0365f003f6be07ed79966ef8a66
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-08 18:18:59 +0100

    Create workpls.lua

---
+++ workpls.lua
@@ -0,0 +1,18 @@
+local counter = 0
+repeat
+  for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.ScrollingFrame:GetChildren() do
+    print("test")
+    if v:IsA("Frame") then
+      print(v.Name)
+      if v.Name~="PetTemplate" then
+        local petpart = v
+        print(petpart.Name)
+        game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("UnequipPet", petpart.Name)
+        task.wait(0.1)
+        counter+=1
+        game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("EquipPet", petpart.Name, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
+      end
+    end
+  end
+  task.wait(0.01)
+until counter >= 545
