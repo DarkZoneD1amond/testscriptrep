29de344878e7b1a1dbf06db5a56fc86e0d404780
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-12 21:50:30 +0100

    Create hedge2.lua

---
+++ hedge2.lua
@@ -0,0 +1,16 @@
+local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+
+while true do
+  for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.PetDisplay.ScrollingFrame:GetChildren() do
+    if v:IsA("Frame") then
+      if v.Name~="PetTemplate" then
+        if v.Main.PET_TYPE.Text ~= "Blood Hedgehog" then return end
+        local petpart = v
+        game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("UnequipPet", petpart.Name)
+        task.wait(0.5)
+        game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("EquipPet", petpart.Name, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
+      end
+    end
+  end
+  task.wait(14.5)
+end
