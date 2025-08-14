08a195df40a88846568bb1da81837dcb8c3b63c8
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-14 23:18:29 +0100

    Create LBHWECHOV2.lua

---
+++ LBHWECHOV2.lua
@@ -0,0 +1,31 @@
+local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+print("initialising")
+task.spawn(function()
+  firesignal(game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame:WaitForChild("Opener").SENSOR.MouseButton1Click)
+end)
+
+while true do
+  for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.PetDisplay.ScrollingFrame:GetChildren() do
+    if v:IsA("Frame") then
+      if v.Name~="PetTemplate" then
+        if string.find(v.Main.PET_TYPE.Text, "Echo Frog") then
+          repeat
+            task.wait()
+          until string.find(v.Main.Cooldowns.COOLDOWN_1.COOLDOWN_NAME.text, "8")
+        end
+      end
+    end
+  end
+  for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.PetDisplay.ScrollingFrame:GetChildren() do
+    if v:IsA("Frame") then
+      if v.Name~="PetTemplate" then
+        if v.Main.PET_TYPE.Text == "Blood Hedgehog" then
+          local petpart = v
+          game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("UnequipPet", petpart.Name)
+          task.wait(0.1)
+          game:GetService("ReplicatedStorage"):WaitForChild("GameEvents"):WaitForChild("PetsService"):FireServer("EquipPet", petpart.Name, game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
+        end
+      end
+    end
+  end
+end
