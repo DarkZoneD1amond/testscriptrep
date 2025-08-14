7bc5b13d61f4691e48aecdbf0107a6d70ecdf3ed
DarkZoneD1amond <ThrowAway911.real@gmail.com>
2025-08-14 23:18:13 +0100

    Update LBHWECHO.lua

--- LBHWECHO.lua
+++ LBHWECHO.lua
@@ -1,19 +1,16 @@
 local pos = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
 print("initialising")
+task.spawn(function()
+  firesignal(game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame:WaitForChild("Opener").SENSOR.MouseButton1Click)
+end)
 
 while true do
   for b,v in game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Main.PetDisplay.ScrollingFrame:GetChildren() do
     if v:IsA("Frame") then
       if v.Name~="PetTemplate" then
         if string.find(v.Main.PET_TYPE.Text, "Echo Frog") then
-          if game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Opener then
-            firesignal(game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Opener.SENSOR.MouseButton1Click)
-          end
           repeat
             task.wait()
-            if game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Opener then
-              firesignal(game.Players.LocalPlayer.PlayerGui.ActivePetUI.Frame.Opener.SENSOR.MouseButton1Click)
-            end
           until string.find(v.Main.Cooldowns.COOLDOWN_1.COOLDOWN_NAME.text, "8")
         end
       end
